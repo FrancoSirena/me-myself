8d805cb94363a360ecf9762f92e2ba75a7529907
Franco Sirena <franco.sirena@bitsighttech.com>
2019-08-26 09:48:00 +0100

    Create a tooltip component

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGit,
@@ -9,6 +8,7 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
 
+import Tooltip from "./Tooltip";
 import LinkOffsite from "./LinkOffsite";
 import withDefaultAnimation from "./helpers/withDefaultAnimation";
 
@@ -33,68 +33,6 @@ const social = {
   }
 };
 
-function SocialList({ parent, onDestroy }) {
-  const [container, setContainer] = useState(undefined);
-  const self = useRef(null);
-  useEffect(() => {
-    let event;
-    if (self) {
-      event = e => {
-        if (
-          e.currentTarget !== self.current &&
-          e.currentTarget !== parent.current
-        ) {
-          onDestroy();
-        }
-      };
-      window.addEventListener("click", event);
-    }
-    return () => {
-      event && window.removeEventListener("click", event);
-    };
-  }, [self, onDestroy, parent]);
-
-  useEffect(() => {
-    if (!container) {
-      const newElement = document.createElement("div");
-      newElement.style.position = "absolute";
-      newElement.style.left = `${parent.current.getBoundingClientRect().left -
-        100}px`;
-      newElement.style.top = `${parent.current.getBoundingClientRect().top -
-        160}px`;
-      document.body.appendChild(newElement);
-      setContainer(newElement);
-    }
-    return () => {
-      container && document.body.removeChild(container);
-    };
-  }, [container, parent]);
-  if (!container) {
-    return null;
-  }
-  return ReactDOM.createPortal(
-    <section className="social-container" ref={self}>
-      <ul>
-        {Object.entries(social).map(([key, { icon, url }]) => (
-          <li key={key}>
-            <LinkOffsite href={url}>
-              <div className="social-item">
-                <FontAwesomeIcon
-                  size="2x"
-                  className="social-icon"
-                  icon={icon}
-                />{" "}
-                {key}
-              </div>
-            </LinkOffsite>
-          </li>
-        ))}
-      </ul>
-    </section>,
-    container
-  );
-}
-
 function Social() {
   const [visible, setVisible] = useState(false);
   const parent = useRef();
@@ -107,7 +45,26 @@ function Social() {
     >
       <FontAwesomeIcon icon={faAddressCard} size="4x" />
       {visible && (
-        <SocialList parent={parent} onDestroy={() => setVisible(v => !v)} />
+        <Tooltip parent={parent} onDestroy={() => setVisible(v => !v)}>
+          <section className="social-container">
+            <ul>
+              {Object.entries(social).map(([key, { icon, url }]) => (
+                <li key={key}>
+                  <LinkOffsite href={url}>
+                    <div className="social-item">
+                      <FontAwesomeIcon
+                        size="2x"
+                        className="social-icon"
+                        icon={icon}
+                      />{" "}
+                      {key}
+                    </div>
+                  </LinkOffsite>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </Tooltip>
       )}
     </button>
   );
