d43f23b4b62f485d35e6869ac9dfb647a546b993
Franco Sirena <franco.sirena@bitsighttech.com>
2020-05-02 23:17:00 +0100

    Fix router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import Logo from "./components/Logo";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
@@ -9,17 +8,15 @@ import "./App.scss";
 function App() {
   return (
     <div className="app-container">
-      <BrowserRouter>
-        <svg viewBox="0 0 100 100" className="triangle">
-          <polygon id="e1_polygon" points="0 0, 0 100, 100 0" />
-        </svg>
-        <svg viewBox="0 0 100 100" className="triangleB">
-          <polygon id="e1_polygon" points="100 0, 100 100, 0 100" />
-        </svg>
-        <Logo />
-        <Home />
-        <Footer />
-      </BrowserRouter>
+      <svg viewBox="0 0 100 100" className="triangle">
+        <polygon id="e1_polygon" points="0 0, 0 100, 100 0" />
+      </svg>
+      <svg viewBox="0 0 100 100" className="triangleB">
+        <polygon id="e1_polygon" points="100 0, 100 100, 0 100" />
+      </svg>
+      <Logo />
+      <Home />
+      <Footer />
     </div>
   );
 }
